0061decc0ff9b445432e4e307b593ff92bbfccfd
Fabiano Beselga <fabianobeselga@gmail.com>
2015-11-26 21:42:39 -0200

    trigger automated build

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ redis_domain = ENV['REDIS_PORT_6379_TCP_ADDR']
 redis_port   = ENV['REDIS_PORT_6379_TCP_PORT']
 redis_url = "redis://#{redis_domain}:#{redis_port}"
 
+# hello
 $redis = Redis.new(:url => redis_url)
 
 get '/' do
